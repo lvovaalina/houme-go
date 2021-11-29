cc28839fadbf73c1e1cbbcc26c5e7ae6e2e8b703
Alina <imlvovaalina@gmail.com>
2021-11-29 19:15:12 +0400

    merge conflict

--- helpers/construction-cost-duration-helper.go
+++ helpers/construction-cost-duration-helper.go
@@ -51,6 +51,7 @@ func CalculateCostDurationForProjectJobs(
 
 		constrDurInHours := int(math.Round(float64(constrDur)))
 		constrDurInDays := int(float64(constrDurInHours) / workingHoursInDay)
+
 		if (constrDurInHours % workingHoursInDay) > 0 {
 			constrDurInDays += 1
 		}
