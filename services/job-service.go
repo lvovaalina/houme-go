d9e66a618ebe9198250aea4af6c97aab74709b46
Alina <imlvovaalina@gmail.com>
2021-12-29 14:53:24 +0100

    HM-279: Remove stairs job if its not required.

--- services/job-service.go
+++ services/job-service.go
@@ -6,6 +6,29 @@ import (
 	"bitbucket.org/houmeteam/houme-go/repositories"
 )
 
+func FindProjectJobs(repository *repositories.JobRepository, project models.Project) dtos.Response {
+	var stairsNumber float32
+	for _, prop := range project.ProjectProperties {
+		if prop.PropertyCode == "SN" {
+			stairsNumber = prop.PropertyValue
+		}
+	}
+	operationResult := repository.FindProjectJobs(
+		project.WallMaterial,
+		project.FoundationMaterial,
+		project.RoofingMaterial,
+		project.FinishMaterial,
+		stairsNumber > 0)
+
+	if operationResult.Error != nil {
+		return dtos.Response{Success: false, Message: operationResult.Error.Error()}
+	}
+
+	var datas = operationResult.Result.(*[]models.Job)
+
+	return dtos.Response{Success: true, Data: datas}
+}
+
 func FindAllJobs(repository *repositories.JobRepository) dtos.Response {
 	operationResult := repository.FindAll()
 
